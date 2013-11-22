0737cd62d8673523cbb6163363e66ed86018cbfd
jasper Koehorst <jasperkoehorst@jaspers-MacBook-Pro.local>
2013-11-22 20:58:52 +0100

    Code for mac simplecv

---
+++ SimpleCV/Mac/camera.py
@@ -0,0 +1,33 @@
+from SimpleCV import *
+from operator import add
+
+import time
+
+cam = Camera()
+
+display=Display()     # essential addition
+time.sleep(5)     # essential addition
+
+ 
+
+normaldisplay = True
+ 
+while display.isNotDone():
+ 
+	if display.mouseRight:
+		normaldisplay = not(normaldisplay)
+		print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
+	
+	img = cam.getImage().flipHorizontal()
+	dist = img.colorDistance(SimpleCV.Color.WHITE).dilate(2)
+	segmented = dist.stretch(200,255)
+	blobs = segmented.findBlobs()
+	if blobs:
+		circles = blobs.filter([b.isCircle(0.2) for b in blobs])
+		if circles:
+			img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
+ 
+	if normaldisplay:
+		img.show()
+	else:
+		segmented.show()
