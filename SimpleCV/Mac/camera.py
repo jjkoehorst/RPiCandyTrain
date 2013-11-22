a0d04e1ae49ca342f29a8ba2a2d1f715c4750b14
jasper Koehorst <jasperkoehorst@jaspers-MacBook-Pro.local>
2013-11-22 22:28:28 +0100

    Line detection with angle

--- SimpleCV/Mac/camera.py
+++ SimpleCV/Mac/camera.py
@@ -1,33 +1,52 @@
-from SimpleCV import *
-from operator import add
+#from SimpleCV import *
+#from operator import add
 
 import time
 
-cam = Camera()
+#cam = Camera()
 
-display=Display()     # essential addition
-time.sleep(5)     # essential addition
+#display=Display()     # essential addition
+
 
  
 
 normaldisplay = True
  
+import SimpleCV
+ 
+cam = SimpleCV.Camera()
+display = SimpleCV.Display()
+time.sleep(5)     # essential addition MAC
+normaldisplay = True
+
 while display.isNotDone():
  
-	if display.mouseRight:
-		normaldisplay = not(normaldisplay)
-		print "Display Mode:", "Normal" if normaldisplay else "Segmented" 
+	try:
+		img = cam.getImage().flipHorizontal()
+		###
+		lines = img.findLines()
+		corners = img.findCorners()
+		for line in lines:
+			if abs(line.angle()) > 45:
+				meanColor = line.meanColor()
+				print meanColor
+				line.draw(SimpleCV.Color.RED) #outline the line segments in red
+		
+		#left_side_corners = corners.filter(corners.x() < img.width / 2)
+		#only look at corners on the left half of the image
+		
+		longest_line = lines.sortLength()[0]
+		#get the longest line returned
+		####
+		
 	
-	img = cam.getImage().flipHorizontal()
-	dist = img.colorDistance(SimpleCV.Color.WHITE).dilate(2)
-	segmented = dist.stretch(200,255)
-	blobs = segmented.findBlobs()
-	if blobs:
-		circles = blobs.filter([b.isCircle(0.2) for b in blobs])
-		if circles:
-			img.drawCircle((circles[-1].x, circles[-1].y), circles[-1].radius(),SimpleCV.Color.BLUE,3)
+		if normaldisplay:
+			img.show()
+		else:
+			segmented.show()
+	except:
+		print "error?"
+
+
+
  
-	if normaldisplay:
-		img.show()
-	else:
-		segmented.show()
