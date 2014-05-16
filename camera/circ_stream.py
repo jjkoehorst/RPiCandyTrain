b5194427f0b35411c916ed131810c838d34d1211
Michael <michaelvolpers@gmail.com>
2014-05-16 21:06:00 +0200

    Update circ_stream.py

--- camera/circ_stream.py
+++ camera/circ_stream.py
@@ -55,7 +55,7 @@ with picamera.PiCamera() as camera:
 	edges = cv2.Canny(gray,50,150,apertureSize = 3)
 	minLineLength = 100
 	maxLineGap = 10
-	lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
-	for x1,y1,x2,y2 in lines[0]:
-	    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-	cv2.imwrite('test_lines2.jpg',img) 
+	#lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+	#for x1,y1,x2,y2 in lines[0]:
+	#    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+	#cv2.imwrite('test_lines2.jpg',img) 
